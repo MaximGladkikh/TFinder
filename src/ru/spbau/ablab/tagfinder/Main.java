7eec1eaf3b9b47a91c2f24cde75d64059ba56c1d
master <master@mit-laptop-140.(none)>
2011-07-15 18:46:18 +0400

    First commit

---
+++ src/ru/spbau/ablab/tagfinder/Main.java
@@ -0,0 +1,14 @@
+package ru.spbau.ablab.tagfinder;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: master
+ * Date: 15.07.11
+ * Time: 18:38
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.err.println("fdfs");
+    }
+}
