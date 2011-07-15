7d7792546e72da3690ab7b82418513b724df2340
Maxim Gladkikh <mgnarg@gmail.com>
2011-07-15 19:54:41 +0400

    http://ablab.myjetbrains.com/youtrack/issue/TFINDER-1

--- src/ru/spbau/ablab/tagfinder/Main.java
+++ src/ru/spbau/ablab/tagfinder/Main.java
@@ -9,7 +9,6 @@ package ru.spbau.ablab.tagfinder;
  */
 public class Main {
     public static void main(String[] args) {
-        System.err.println("fdfs");
-
+        System.err.println("hello world!");
     }
 }
