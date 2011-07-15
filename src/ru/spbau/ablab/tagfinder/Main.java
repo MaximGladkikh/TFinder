9a96d87d8c6ef1472c2760dbe8722fd0bf397b8a
Maxim Gladkikh <mgnarg@gmail.com>
2011-07-15 20:21:23 +0400

    #TFINDER-1 fixed tag to be tested

--- src/ru/spbau/ablab/tagfinder/Main.java
+++ src/ru/spbau/ablab/tagfinder/Main.java
@@ -2,6 +2,6 @@ package ru.spbau.ablab.tagfinder;
 
 public class Main {
     public static void main(String[] args) {
-        System.err.println("hello world!!!");
+        System.err.println("hello world!!!v ");
     }
 }
